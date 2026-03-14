81dacb0a5752bba7cb72baf628b4ada97a7842ef
Iam-Zarif <zarifraja19@gmail.com>
2026-03-15 03:31:08 +0600

    add

---
+++ lib/products.ts
@@ -0,0 +1,10 @@
+export type CatalogProduct = {
+  id: string;
+  name: string;
+  brand: string;
+  price: number;
+  rating: number;
+  image: string;
+};
+
+export const catalogProducts: CatalogProduct[] = [];
